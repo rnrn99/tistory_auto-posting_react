92bca265ed7391854a42f93dfb4fcc35ceb36519
giu0213@naver.com <giu0213@naver.com>
2021-10-20 18:16:36 +0900

    [MOD] heroku setting

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   secret: process.env.SECRET,
   chromedriverPath: process.env.CHROMEDRIVER_PATH,
-  Bin: process.env.GOOGLE_CHROME_BIN,
+  chromeBin: process.env.GOOGLE_CHROME_BIN,
   cloudName: process.env.CLOUD_NAME,
   cloudApiKey: process.env.CLOUD_API_KEY,
   cloudApiSecret: process.env.CLOUD_API_SECRET,
--- server/scrape/scrape.js
+++ server/scrape/scrape.js
@@ -182,14 +182,14 @@ exports.getGameURL = async (month, date, teamCode) => {
   let options = new chrome.Options();
 
   if (process.env.NODE_ENV === "production") {
-    options.setChromeBinaryPath(config.Bin);
+    options.setChromeBinaryPath(config.chromeBin);
   }
   options.addArguments("--headless");
   options.addArguments("--disable-gpu");
   options.addArguments("--no-sandbox");
   options.addArguments("window-size=1920,1080");
   options.addArguments(
-    "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
+    "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
   );
 
   browser = new driver.Builder()
