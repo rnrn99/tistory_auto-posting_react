7b66328e4bbeff430683d39914b566cefc6d1655
giu0213@naver.com <giu0213@naver.com>
2021-10-20 22:07:20 +0900

    [TEST] set lang option

--- server/scrape/scrape.js
+++ server/scrape/scrape.js
@@ -195,7 +195,7 @@ exports.getGameURL = async (month, date, teamCode) => {
   options.addArguments("--disable-gpu");
   options.addArguments("--no-sandbox");
   options.addArguments("window-size=1920,1080");
-  options.addArguments("--lang=en");
+  options.addArguments("--lang=ko-KR.UTF-8");
   options.addArguments(
     "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
   );
