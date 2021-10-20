b9d215b67c6e6a12e1f512f48a60f8c1cf8126ca
giu0213@naver.com <giu0213@naver.com>
2021-10-20 17:53:46 +0900

    [MOD] chromedriver window size option

--- server/scrape/scrape.js
+++ server/scrape/scrape.js
@@ -184,12 +184,12 @@ exports.getGameURL = async (month, date, teamCode) => {
   if (process.env.NODE_ENV === "production") {
     options.setChromeBinaryPath(config.Bin);
   }
-  // options.addArguments("--headless");
+  options.addArguments("--headless");
   options.addArguments("--disable-gpu");
   options.addArguments("--no-sandbox");
-  options.addArguments("--start-fullscreen");
+  options.addArguments("window-size=1920,1080");
   options.addArguments(
-    "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
+    "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
   );
 
   browser = new driver.Builder()
