e04d929c25e5b464def0fe54eb62f8570c7098f5
giu0213@naver.com <giu0213@naver.com>
2021-10-21 13:07:23 +0900

    [TEST] font test

--- server/scrape/scrape.js
+++ server/scrape/scrape.js
@@ -201,7 +201,7 @@ exports.getGameURL = async (month, date, teamCode) => {
   options.addArguments("--lang=ko");
   options.addArguments("--font-render-hinting=none");
   options.addArguments(
-    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
+    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
   );
 
   browser = new driver.Builder()
